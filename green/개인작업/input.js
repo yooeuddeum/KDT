c64c6e5387bef97684da2403e16311799609687f
유으뜸 <www.goolge.com>
2024-06-05 18:13:21 +0900

    작업

--- green/개인작업/input.js
+++ green/개인작업/input.js
@@ -1,27 +1,35 @@
-// * 초기화 된 변수 배열에 데이터가 추가 될 곳
-// * 계속 추가 될 수 있는 부분 / 임시로 하나만 설정
-const user = [
-  { userID: "user123", userPW: "pass1515" }
-];
+// * 사용자 데이터를 저장할 빈 배열
+let user = [];
 
-// * 해당 태그들 불러옴
+// 버튼 태그 불러옴
 const button = document.getElementById("btn");
 console.log(button);
 
-// * 해당 id와 pw이 값을 입력했을때 user 데이터 배열에 추가되게끔 유도
-// * userID와 PW가 데이터 타입이 문자열인 형식일때만 적용 / 그게 아니라면 에러가 뜨게끔 적용
+// * 버튼 요소에 클릭 이벤트 부여하고 이벤트가 발생했을때 실행해서
+// * 단발성 기능 수행 / 아이디와 비밀번호가 빈 문자열이 아니라면 실행이 되게끔 수정
+// * 데이터 값이 추가되면 입력창에서 값을 지움
+// * 만약 input창에 아무것도 입력안하고 버튼 클릭시 에러가 뜨게 했음
+// * 마지막 초기화의 기준은 브라우저상에서 새로고침을 하거나 창을 닫을시 초기화 그렇지 않다면 데이터는 저장 된 상태에서 계속 유지
+// * 보안적인 부분에서는 취약한 단점 또한 이 자쓰 코드는 폼 태그 기준에서 서버로 데이터를 보내지 않는 방식임
+// * 그래서 해당 방식은 버튼 type이 button일때 자쓰 자체에서 기능 구현으로 사용 용도
 button.addEventListener("click", () => {
-  const userID = document.getElementById("user_id").value;
+  const userIDInput = document.getElementById("user_id");
+  const userPWInput = document.getElementById("user_pw");
+  const userID = userIDInput.value;
+  const userPW = userPWInput.value;
 
-  const userPW = document.getElementById("user_pw").value;
+  if (userID !== "" && userPW !== "") {
 
-  if (typeof userID === "string" && typeof userPW === "string") {
     user.push({ userID, userPW });
-    console.log("회원가입이 가능한 아이디와 비밀번호 입니다");
-  } else {
-    console.error("userID와 PW가 문자열이 아닙니다.");
+    console.log("회원가입이 가능한 아이디와 비밀번호입니다.");
+    console.log("현재 사용자 데이터:", user);
+
+    userIDInput.value = "";
+    userPWInput.value = "";
+  } 
+  else {
+    console.error("아이디나 비밀번호를 입력해주세요.");
   }
 });
 
-console.log(user)
-console.log("초기화 된 데이터", user);
+console.log("초기화된 데이터", user);
